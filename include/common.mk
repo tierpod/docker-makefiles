975eaa9c51036ca9ee8d074ef56e96117eb8bcf7
Pavel Podkorytov <pod.pavel@gmail.com>
2015-06-16 18:00:07 +0500

    Rename include/Makefile -> include/common.mk

--- include/common.mk
+++ include/common.mk
@@ -1,7 +1,7 @@
 USERID = $(shell id -u)
 GROUPID = $(shell id -g)
 
-.PHONY: all show clean package shell
+.PHONY: all show clean package shell prepare
 
 ifndef BUILD_NUMBER
 $(info BUILD_NUMBER set to 0)
@@ -28,6 +28,9 @@ Dockerfile:
 
 image: show Dockerfile
 	docker build -t $(IMAGE) .
+ifneq ($(BUILD_NUMBER), 0)
+	docker tag $(IMAGE):latest $(IMAGE):$(BUILD_NUMBER)
+endif
 
 clean:
 	rm -rf Dockerfile build-env
@@ -35,10 +38,10 @@ clean:
 build-env:
 	cp -r volume build-env
 
-package: build-env
+package: build-env prepare
 	docker run --rm -v $(PWD)/build-env/:/home/builder/build \
 		-e BUILD_NUMBER=$(BUILD_NUMBER) -e TARGET=$(TARGET) -t $(IMAGE)
 
-shell: build-env
+shell: build-env prepare
 	docker run -it --rm -v $(PWD)/build-env/:/home/builder/build \
 		-e BUILD_NUMBER=$(BUILD_NUMBER) -e TARGET=$(TARGET) -t $(IMAGE) /bin/bash
