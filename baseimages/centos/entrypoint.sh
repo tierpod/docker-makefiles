cef9cf6be76f4287a405f751c730d5c8326e004d
Pavel Podkorytov <pod.pavel@gmail.com>
2016-07-08 17:34:36 +0500

    WIP: Try new scripts
    1. Create builder user after container starting
    2. Remove Dockerfile.template
    3. Build image with --build-arg TARGET=package.spec

--- baseimages/centos/entrypoint.sh
+++ baseimages/centos/entrypoint.sh
@@ -1,14 +1,44 @@
-#!/bin/sh
+#!/bin/bash
 
-set -xe
+set -e
+
+usage() {
+	echo 'Usage: USER=builder USERID=1000 GROUPID=1000 TARGET=package.spec entrypoint.sh'
+}
+
+runas() {
+	su $GUEST_USER -c "$@"
+}
+
+create_user() {
+	echo "Create user: $GUEST_USER ($GUEST_USERID:$GUEST_GROUPID)"
+	groupadd -g $GUEST_GROUPID $GUEST_USER
+	useradd -m -s /bin/bash -u $GUEST_USERID -g $GUEST_GROUPID $GUEST_USER
+}
+
+build() {
+	echo "Prepare rpmbuild directory tree"
+	touch /home/$GUEST_USER/.rpmmacros
+	chown $GUEST_USERID:$GUEST_GROUPID /home/$GUEST_USER/.rpmmacros
+	runas "rpmdev-setuptree"
+	cd /home/$GUEST_USER/rpmbuild
+
+	echo "Download sources"
+	[ -d 'SOURCES' ] || runas "mkdir SOURCES"
+	runas "spectool -g -R SPECS/$TARGET"
+
+	echo "Build rpm package from spec $TARGET"
+	runas "rpmbuild --define 'release $RELEASE' -ba SPECS/$TARGET"
+}
+
+GUEST_USER=${USER:-builder}
+GUEST_USERID=${USERID:-1000}
+GUEST_GROUPID=${GROUPID:-1000}
 
 if [ -n "$TARGET" ]; then
-	# Download source
-	[ -d 'SOURCES' ] || mkdir SOURCES
-	spectool -g -R SPECS/$TARGET
-	# Build rpm package
-	rpmbuild -ba SPECS/$TARGET
+	create_user
+	build
 else
-	echo 'Usage: TARGET=package.spec entrypoint.sh'
+	usage
 	exit 1
 fi
--- include/common.mk
+++ include/common.mk
@@ -1,55 +1,63 @@
+# Variables
 USERID := $(shell id -u)
 GROUPID := $(shell id -g)
+DOCKERFILE := Dockerfile
 
 BUILD_NUMBER ?= 0
-TARGET ?= $(error TARGET is not defined)
 IMAGE ?= $(error IMAGE is not defined)
 
 ifdef COMMIT
-RELEASE = $(BUILD_NUMBER).git$(COMMIT)
+RELEASE := $(BUILD_NUMBER).git$(COMMIT)
 else
-RELEASE = $(BUILD_NUMBER)
+RELEASE := $(BUILD_NUMBER)
 endif
 
-TMP_DIR = /docker-home/$(IMAGE)
-DOCKER_VOLUME = -v $(TMP_DIR)/volume:/home/builder/rpmbuild
-DOCKER_ENV = -e TARGET=$(TARGET) -e RELEASE=$(RELEASE)
+TMP_DIR := /docker-home/$(IMAGE)
+DOCKER_VOLUME := -v $(TMP_DIR)/volume:/home/builder/rpmbuild
+DOCKER_ENV := -e RELEASE=$(RELEASE) -e USERID=$(USERID) -e GROUPID=$(GROUPID)
 
-.PHONY: all show clean clean-all build-env package shell prepare
+ifdef TARGET
+DOCKER_BUILDARG := --build-arg TARGET=$(TARGET)
+DOCKER_ENV := -e TARGET=$(TARGET) $(DOCKER_ENV)
+else
+DOCKER_BUILDARG :=
+endif
+
+# Targets
+.PHONY: all show clean clean-image build-env package shell prepare
 
 all: show image package
 
 show:
 	@echo '-------------------------------------------------------'
-	@echo 'USERID=$(USERID) GROUPID=$(GROUPID) RELEASE=$(RELEASE)'
-	@echo 'IMAGE=$(IMAGE) TARGET=$(TARGET) COMMIT=$(COMMIT)'
+	@echo '          IMAGE $(IMAGE)'
+	@echo '     DOCKERFILE $(DOCKERFILE)'
+	@echo '     DOCKER_ENV $(DOCKER_ENV)'
+	@echo '  DOCKER_VOLUME $(DOCKER_VOLUME)'
+	@echo 'DOCKER_BUILDARG $(DOCKER_BUILDARG)'
 	@echo '-------------------------------------------------------'
 
-Dockerfile:
-	@cat Dockerfile.template \
-		| sed -e "s@{userid}@$(USERID)@" -e "s@{groupid}@$(GROUPID)@" -e "s@{target}@$(TARGET)@" \
-		| tee $@
-
-image: show Dockerfile
-	docker build -t $(IMAGE) .
+image: show
+	docker build $(DOCKER_BUILDARG) -t $(IMAGE) -f $(DOCKERFILE) .
 ifneq ($(BUILD_NUMBER), 0)
 	docker tag $(IMAGE):latest $(IMAGE):$(BUILD_NUMBER)
 endif
 
 clean:
-	rm -rf Dockerfile $(TMP_DIR)
+	rm -rf $(TMP_DIR)
 
-clean-all: clean
+clean-image: clean
 	-docker rmi $(IMAGE):$(BUILD_NUMBER)
 
 build-env: $(TMP_DIR)
 	-cp -r volume $(TMP_DIR)
 
-package: build-env prepare
+package: show build-env prepare
 	docker run --rm $(DOCKER_VOLUME) $(DOCKER_ENV) $(IMAGE)
 
-shell: build-env prepare
+shell: show build-env prepare
 	docker run --rm -it -u root $(DOCKER_VOLUME) $(DOCKER_ENV) $(IMAGE) /bin/bash
 
 $(TMP_DIR):
 	mkdir -p $@
+
